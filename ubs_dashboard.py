1c6a733ab1bb1785756d7db0b81a92b926073a02
Pedro Caixeta <pedro.mg.caixeta@gmail.com>
2025-02-26 14:05:00 -0300

    trabalho finalizado

--- ubs_dashboard.py
+++ ubs_dashboard.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # Carregar o arquivo atualizado
 df = pd.read_csv("ubs_atualizado.csv", sep=";")
 
-# Converter latitude e longitude para float
+# Exercicio 1: Mapa Interativo das UBS por Estado
 df['LATITUDE'] = pd.to_numeric(df['LATITUDE'].str.replace(',', '.'), errors='coerce')
 df['LONGITUDE'] = pd.to_numeric(df['LONGITUDE'].str.replace(',', '.'), errors='coerce')
 
@@ -45,3 +45,29 @@ if 'LATITUDE' in df.columns and 'LONGITUDE' in df.columns:
         st.map(df_mapa)
 else:
     st.error("As colunas 'latitude' e 'longitude' não foram encontradas no dataset.")
+
+#Exercicio 2: Gráfico de Pizza da Distribuição de UBS por Estado
+grafico = px.pie(df_freq, names='Estado', values='Frequência', title='UBS por Estado')
+st.plotly_chart(grafico)
+
+#Exercicio 3: Histograma da Quantidade de UBS por Município
+# Contar a quantidade de UBS por município
+df_municipios = df['Nome_Município'].value_counts().reset_index()
+df_municipios.columns = ['Município', 'Quantidade_UBS']
+
+# Controle deslizante para filtrar municípios com um número mínimo de UBS
+min_ubs = st.slider("Selecione o número mínimo de UBS por município", 
+                    min_value=int(df_municipios['Quantidade_UBS'].min()), 
+                    max_value=int(df_municipios['Quantidade_UBS'].max()), 
+                    value=int(df_municipios['Quantidade_UBS'].min()))
+
+# Filtrar os municípios
+df_filtrado = df_municipios[df_municipios['Quantidade_UBS'] >= min_ubs]
+
+# Criar gráfico de barras para visualizar os municípios com o filtro aplicado
+grafico_barras = px.bar(df_filtrado, x='Município', y='Quantidade_UBS', 
+                        title="Número de UBS por Município",
+                        labels={'Município': 'Município', 'Quantidade_UBS': 'Número de UBS'},
+                        text_auto=True)
+
+st.plotly_chart(grafico_barras)
